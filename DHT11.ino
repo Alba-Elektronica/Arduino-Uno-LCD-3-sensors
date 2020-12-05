ab503cde6f9edf709e7e82d72cf127dc3dc52372
albaelektronica <64310350+albaelektronica@users.noreply.github.com>
2020-12-05 08:04:04 +0100

    Create DHT11.ino

---
+++ DHT11.ino
@@ -0,0 +1,71 @@
+// Alba Elektronica @ 2020
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+#define DHTTYPE DHT11   // DHT 11
+
+#define DHTPIN 2
+DHT dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+// LCD pinout
+// A5  SCL, A4  SDA, 5V VCC, GND  GND
+// KY-015 pinout
+// S  2,  +  3.3V,  -  GND 
+
+void setup(){
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+  
+  Serial.begin(9600);
+  
+  dht.begin();
+  
+}
+
+void loop()
+{
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  lcd.setCursor(0,0); 
+  lcd.print("Temp: ");
+  lcd.print(t);
+  lcd.print((char)223);
+  lcd.print("C");
+
+  lcd.setCursor(0,1);
+  lcd.print("Humidity max: ");
+  lcd.print(h);
+  lcd.print("%");
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+
+  // Set delay between sensor readings based on sensor details.
+  delay(10000);
+}
